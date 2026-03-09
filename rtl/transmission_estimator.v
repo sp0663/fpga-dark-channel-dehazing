160b3c814b04058cbd83b5a28171336ff3c39e03
sp0663 <sp2410663@gmail.com>
2026-03-09 11:08:55 +0530

    Refactor transmission_estimator for improved logic

--- rtl/transmission_estimator.v
+++ rtl/transmission_estimator.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 01/16/2026 09:19:47 PM
+// Create Date: 03/08/2026 11:47:42 AM
 // Design Name: 
 // Module Name: transmission_estimator
 // Project Name: 
@@ -21,20 +21,61 @@
 
 
 module transmission_estimator(
-    input i_clk,
-    input i_rst,
-    input [7:0] i_data,     //dark channel value J(x)
-    input i_data_valid,
-    input [7:0] A_min,
-    output [7:0] o_data,    //transmission map value t(x)
-    output o_data_valid
- );
-    
-wire [15:0] temp;
-wire [7:0] t0 = 8'd153;
+    input wire i_clk,
+    input wire i_rst,
+    input wire [7:0] i_data,      // dark channel value J(x)
+    input wire i_data_valid,
+    input wire [7:0] A_min,
+    output reg [7:0] o_data,      // transmission map value t(x)
+    output reg o_data_valid
+);
 
-assign temp = (i_data * 243) / A_min;
-assign o_data = (temp < t0) ? t0 : (256 - temp[7:0]);
-assign o_data_valid = i_data_valid;
+    reg [15:0] inv_A_rom [0:255];
+    integer i;
+    initial begin
+        inv_A_rom[0] = 16'd0; // Prevent divide-by-zero crashes
+        for (i = 1; i < 256; i = i + 1) begin
+            // Calculates (243 * 256) / A_min
+            inv_A_rom[i] = (243 * 256) / i; 
+        end
+    end
+
+    reg [7:0]  data_reg;
+    reg [15:0] inv_A_reg;
+    reg        valid_reg1, valid_reg2;
+    reg [23:0] mult_result;
     
+    wire [15:0] temp;
+    wire [7:0]  t0 = 8'd153;
+
+    assign temp = mult_result[23:8]; 
+
+    always @(posedge i_clk) begin
+        if (i_rst) begin
+            o_data <= 8'd0;
+            o_data_valid <= 1'b0;
+            valid_reg1 <= 1'b0;
+            valid_reg2 <= 1'b0;
+            data_reg <= 8'd0;
+            inv_A_reg <= 16'd0;
+            mult_result <= 24'd0;
+        end else begin
+            inv_A_reg  <= inv_A_rom[A_min];
+            data_reg   <= i_data;
+            valid_reg1 <= i_data_valid;
+
+            mult_result <= data_reg * inv_A_reg;
+            valid_reg2  <= valid_reg1;
+
+            if (temp < t0) begin
+                o_data <= t0;
+            end else begin
+                o_data <= 9'd256 - temp[7:0]; 
+            end
+            
+            o_data_valid <= valid_reg2;
+        end
+    end
+
 endmodule
+
