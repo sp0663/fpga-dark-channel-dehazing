55fee68c55633bc6bd2a99902f97c25c37aebec8
sp0663 <sp2410663@gmail.com>
2026-03-09 11:04:51 +0530

    Update lineBuffer.v to 7x7

--- rtl/lineBuffer.v
+++ rtl/lineBuffer.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 01/15/2026 07:08:29 PM
+// Create Date: 03/08/2026 11:45:56 AM
 // Design Name: 
 // Module Name: lineBuffer
 // Project Name: 
@@ -25,7 +25,7 @@ module lineBuffer(
     input   i_rst,
     input [7:0] i_data,
     input   i_data_valid,
-    output [119:0] o_data,
+    output [55:0] o_data,
     input i_rd_data
 );
 
@@ -54,15 +54,7 @@ assign o_data = {
     line[(rdPntr+3) & 9'h1ff],
     line[(rdPntr+4) & 9'h1ff],
     line[(rdPntr+5) & 9'h1ff],
-    line[(rdPntr+6) & 9'h1ff],
-    line[(rdPntr+7) & 9'h1ff],
-    line[(rdPntr+8) & 9'h1ff],
-    line[(rdPntr+9) & 9'h1ff],
-    line[(rdPntr+10) & 9'h1ff],
-    line[(rdPntr+11) & 9'h1ff],
-    line[(rdPntr+12) & 9'h1ff],
-    line[(rdPntr+13) & 9'h1ff],
-    line[(rdPntr+14) & 9'h1ff]
+    line[(rdPntr+6) & 9'h1ff]
 };
 
 always @(posedge i_clk)
@@ -74,3 +66,5 @@ begin
 end
 
 endmodule
+
+
