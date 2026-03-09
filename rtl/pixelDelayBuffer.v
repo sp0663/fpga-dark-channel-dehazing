adf896bbb8995c77731729806fff4ba29656f72e
sp0663 <sp2410663@gmail.com>
2026-03-09 11:07:50 +0530

    Update pixelDelayBuffer to 7x7

--- rtl/pixelDelayBuffer.v
+++ rtl/pixelDelayBuffer.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 01/17/2026 01:01:43 PM
+// Create Date: 03/08/2026 11:48:17 AM
 // Design Name: 
 // Module Name: pixelDelayBuffer
 // Project Name: 
@@ -21,48 +21,45 @@
 
 
 module pixelDelayBuffer(
-    input i_clk,
-    input i_rst,
-    input [23:0] i_pixel,        // RGB pixel input
-    input i_pixel_valid,
-    output [23:0] o_pixel,       // Delayed RGB pixel output
-    output reg o_pixel_valid
+    input  i_clk,
+    input  i_rst,
+    input  [23:0] i_pixel,
+    input  i_pixel_valid,
+    output [23:0] o_pixel,
+    output        o_pixel_valid
 );
 
 reg [31:0] fill_counter;
-wire [23:0] pixel_chain [0:14];  
 
-// 15 line buffers (matching lineBufferArray)
+wire [23:0] pixel_chain  [0:6];
+wire        valid_chain  [0:6];  
+
 genvar i;
 generate
-    for(i = 0; i < 15; i = i + 1) begin
+    for(i = 0; i < 7; i = i + 1) begin : plb_gen
         pixelLineBuffer plb_inst (
-            .i_clk(i_clk),
-            .i_rst(i_rst),
-            .i_data(i == 0 ? i_pixel : pixel_chain[i-1]),
-            .i_data_valid(i_pixel_valid),
-            .o_data(pixel_chain[i]),
-            .i_rd_data(1'b1)
+            .i_clk       (i_clk),
+            .i_rst       (i_rst),
+            .i_data      (i == 0 ? i_pixel        : pixel_chain[i-1]),
+            .i_data_valid(i == 0 ? i_pixel_valid  : valid_chain[i-1]),  
+            .o_data      (pixel_chain[i]),
+            .o_data_valid(valid_chain[i]),
+            .i_rd_data   (1'b1)
         );
     end
 endgenerate
 
-assign o_pixel = pixel_chain[0];
+assign o_pixel = pixel_chain[6];  
+
+localparam FILL_THRESHOLD = (512 * 7) + 7 - 1;
 
 always @(posedge i_clk) begin
-    if (i_rst)
-        fill_counter <= 0;
-    else if (i_pixel_valid && fill_counter < ((512*15)-1))
-        fill_counter <= fill_counter + 1;
+    if(i_rst)
+        fill_counter <= 'd0;
+    else if(i_pixel_valid && fill_counter < FILL_THRESHOLD)
+        fill_counter <= fill_counter + 'd1;
 end
 
-always @(posedge i_clk) begin
-    if (i_rst)
-        o_pixel_valid <= 1'b0;
-    else if (fill_counter >= ((512*15)-1))
-        o_pixel_valid <= i_pixel_valid;
-    else
-        o_pixel_valid <= 1'b0;
-end
+assign o_pixel_valid = (fill_counter >= FILL_THRESHOLD) && valid_chain[6];
 
 endmodule
