0e79d22477bf827a857627e6e040e9adac12d017
sp0663 <sp2410663@gmail.com>
2026-03-09 15:14:50 +0530

    Add pixel validity check to rgb_min module
    Added input signal for pixel validity and updated logic to handle invalid pixels.

--- rtl/rgb_min.v
+++ rtl/rgb_min.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 01/15/2026 06:32:19 PM
+// Create Date: 03/08/2026 11:37:43 AM
 // Design Name: 
 // Module Name: rgb_min
 // Project Name: 
@@ -22,20 +22,25 @@
 
 module rgb_min(
     input [23:0] i_pixel_data,      //rgb values of the pixel
+    input i_pixel_valid,
     output [7:0] o_min              //min of rgb
     );
     
     reg [7:0] curr_min;
     
-    always @(*)
-    begin
-        curr_min = i_pixel_data[23:16];
-        if(curr_min > i_pixel_data[15:8])
-            curr_min = i_pixel_data[15:8];
-        if(curr_min > i_pixel_data[7:0])
-            curr_min = i_pixel_data[7:0];
+    always @(*) begin
+        if(i_pixel_valid) begin
+            curr_min = i_pixel_data[23:16];
+            if(curr_min > i_pixel_data[15:8])
+                curr_min = i_pixel_data[15:8];
+            if(curr_min > i_pixel_data[7:0])
+                curr_min = i_pixel_data[7:0];
+        end
+        else
+            curr_min = 8'd0;
     end
     
     assign o_min = curr_min;
     
 endmodule
+
